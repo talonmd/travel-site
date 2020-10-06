c8376f6494c7fa3dec3ca3506f9c95460adb25de
Talon Durrant <talonmdurrant@gmail.com>
2020-10-06 03:25:46 -0600

    added postcss mixins plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require("path")
 
-const postCSSPlugins = [require("postcss-import"), require("postcss-simple-vars"), require("postcss-nested"), require("autoprefixer")]
+const postCSSPlugins = [require("postcss-import"), require("postcss-mixins"), require("postcss-simple-vars"), require("postcss-nested"), require("autoprefixer")]
 
 module.exports = {
   entry: "./app/assets/scripts/App.js",
